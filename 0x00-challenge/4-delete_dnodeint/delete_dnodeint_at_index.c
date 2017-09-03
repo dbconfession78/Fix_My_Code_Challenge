2d272d7fb74d151fd9745cc0bb28da606dd6e93a
Stuart Kuredjian <108@holbertonschool.com>
2017-09-03 02:10:50 +0000

    delete_dnodeint_at_index.c :put free back

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -37,15 +37,13 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		*head = tmp;
 		if (tmp != NULL)
 			tmp->prev = NULL;
-
 	}
 	else
 	{
-		/* (*head)->next->prev = (*head)->prev; */
-		/* (*head)->prev->prev = (*head)->prev; */
 		if ((*head)->next)
 			(*head)->next->prev = (*head)->prev;
 		(*head)->prev->next = (*head)->next;
+		free(*head);
 		*head = saved_head;
 	}
 	return (1);
